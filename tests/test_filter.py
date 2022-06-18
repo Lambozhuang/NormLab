45b98e1e0852d2c314539f051c327261627b0d6c
Lambozhuang <lambozhuang@icloud.com>
2022-06-18 22:14:19 +0800

    测试过滤（删除ignore文件并合并同名文件夹）

--- tests/test_filter.py
+++ tests/test_filter.py
@@ -2,8 +2,8 @@ from pathlib import Path
 from filter_tool import FilterTraverseTool
 from archive_tool import UnarchiveTraverseTool
 
-def test_filter_ignore():
-    root_zip_path = Path('./tests/test_filter_ignore/Lab01-中文.zip')
+def test_filter():
+    root_zip_path = Path('./tests/test_filter/Lab01-中文.zip')
 
     archive_tool = UnarchiveTraverseTool()
     archive_tool.handle_zip(root_zip_path)
@@ -11,4 +11,5 @@ def test_filter_ignore():
     root_path = root_zip_path.parent / root_zip_path.stem
 
     filter_tool = FilterTraverseTool()
-    filter_tool.traverse_path(root_path)
+    filter_tool.traverse_path(root_path) # filter ignore
+    filter_tool.traverse_path(root_path) # prune tree
